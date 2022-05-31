005d00c685fd2293e9e08c60b721a436148bd30a
Samuel Pena <inestbht@gmail.com>
2022-05-30 22:06:22 -0500

    Completed - Ch. 3.3 - Enhancing the UI: Working with the Keyboard, Dealing with the firstResponder, and Handling Actions in a UITextField - Exercise 1,2 Two Players

--- Two Players/ViewController.swift
+++ Two Players/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var playerOneLabel: UILabel!
+    @IBOutlet weak var playerTwoLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var playerField: UITextField!
+    @IBOutlet weak var beginGameButton: UIButton!
+    
+    var numberOfPlayers = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playerField.becomeFirstResponder()
+        nameLabel.text = "Player 1: "
     }
-
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        if numberOfPlayers == 0 {
+            playerOneLabel.text = "Player 1: \(playerField.text!)"
+            numberOfPlayers += 1
+            playerField.text = ""
+            nameLabel.text = "Player 2:"
+        } else {
+            playerTwoLabel.text = "Player 2: \(playerField.text!)"
+            playerField.text = ""
+            nameLabel.text = ""
+            playerField.resignFirstResponder()
+            playerField.isEnabled = false
+            beginGameButton.isHidden = false
+        }
+    }
+    
 
 }
 
